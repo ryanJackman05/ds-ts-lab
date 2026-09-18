74c13c33d63376082361eab441893ad751b1073e
ryanJackman05 <20108256@mail.wit.ie>
2026-09-18 13:52:38 +0100

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,3 +1,23 @@
 import {Friend, Colleague } from './myTypes'
 import { friends, colleagues } from "./01-basics";
 
+function older(f: Friend) : string {
+     f.age += 1
+     return `${f.name} is now ${f.age}` 
+}
+
+console.log(older(friends[0]))
+
+function highestExtension(cs: Colleague[]) : Colleague {
+    const result  = cs.sort(
+        (c1, c2) => c1.contact.extension - c2.contact.extension
+    )
+    return result[cs.length - 1]
+}
+console.log(highestExtension(colleagues.current))
+
+function addColleague(cs: Colleague[], name: string, department: string, email: string) : void {
+    cs.push({ name, department, contact: { email, extension: cs.length + 200 } });
+}
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
