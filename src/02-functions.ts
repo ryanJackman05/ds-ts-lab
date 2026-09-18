6d6eefc69889fdcebc05d1a47c93f3f9d49edac3
ryanJackman05 <20108256@mail.wit.ie>
2026-09-18 17:06:34 +0100

    Higher-order functions

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,4 +1,4 @@
-import {Friend, Colleague } from './myTypes'
+import {Friend, Colleague, EmailContact } from './myTypes'
 import { friends, colleagues } from "./01-basics";
 
 function older(f: Friend) : string {
@@ -8,11 +8,11 @@ function older(f: Friend) : string {
 
 console.log(older(friends[0]))
 
-function highestExtension(cs: Colleague[]) : Colleague {
-    const result  = cs.sort(
-        (c1, c2) => c1.contact.extension - c2.contact.extension
-    )
-    return result[cs.length - 1]
+function highestExtension(cs: Colleague[]) { // Inferred retun type
+  const result = cs.sort(
+    (c1, c2) => c1.contact.extension - c2.contact.extension
+  );
+  return result[cs.length - 1];
 }
 console.log(highestExtension(colleagues.current))
 
@@ -21,3 +21,23 @@ function addColleague(cs: Colleague[], name: string, department: string, email: 
 }
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+
+function sortColleagues(
+  colleagues: Colleague[],
+  sorter: (c1: Colleague, c2: Colleague) => number
+): EmailContact[] {
+  const sorted = colleagues.sort(sorter); // Colleague[] inferred
+  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return result 
+}
+console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
+console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+
+function findFriends(
+    friends: Friend[],
+    condition: (f: Friend) => boolean
+): Friend[] {
+    return friends.filter(condition);
+}
+console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
+console.log(findFriends(friends, (friend) => friend.age < 35));
--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -12,9 +12,14 @@ export interface Colleague {
       email: string;
       extension: number
     } 
-  }
+}
 
 export interface ColleagueHistory {
   current: Colleague[],
   former: Colleague[]
+}
+
+export interface EmailContact {
+    name: string;
+    email: string
 }
