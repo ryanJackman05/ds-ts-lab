01945aaaeb768eb34a4fb1447421dc007f1d7c52
ryanJackman05 <20108256@mail.wit.ie>
2026-09-19 12:49:38 +0100

    Demo optional properties and parameters.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -5,7 +5,7 @@ function older(f: Friend) : string {
      f.age += 1
      return `${f.name} is now ${f.age}` 
 }
-
+console.log("\n\nOLDER")
 console.log(older(friends[0]))
 
 function highestExtension(cs: Colleague[]) { // Inferred retun type
@@ -14,24 +14,35 @@ function highestExtension(cs: Colleague[]) { // Inferred retun type
   );
   return result[cs.length - 1];
 }
+console.log("\n\nHIGHEST EXTENSION")
 console.log(highestExtension(colleagues.current))
 
 function addColleague(cs: Colleague[], name: string, department: string, email: string) : void {
     cs.push({ name, department, contact: { email, extension: cs.length + 200 } });
 }
 addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log("\n\nADD COLLEAGUE")
 console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
 
 function sortColleagues(
   colleagues: Colleague[],
-  sorter: (c1: Colleague, c2: Colleague) => number
+  sorter: (c1: Colleague, c2: Colleague) => number,
+  max? : number // optional parameters can be written right into the declaration
 ): EmailContact[] {
-  const sorted = colleagues.sort(sorter); // Colleague[] inferred
-  const result: EmailContact[] = sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
-  return result 
+  let end = colleagues.length
+  if (max !== undefined) {
+    end = max < 2 ? 1 : max
+  }
+  const sorted = colleagues.sort(sorter);
+  const fullResult =  sorted.map((ce) => ({ name: ce.name, email: ce.contact.email }));
+  return fullResult.slice(0,end)
 }
-console.log(sortColleagues(colleagues.current, (a, b) => a.contact.extension - b.contact.extension));
-console.log(sortColleagues(colleagues.current, (a, b) => a.name.length - b.name.length));
+// Test invocations
+console.log("\n\nSORT COLLEAGUES")
+console.log(sortColleagues(colleagues.current, (a, b) => (a.contact.extension - b.contact.extension),3));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length),1));
+console.log(sortColleagues(colleagues.current, (a, b) => (a.name.length - b.name.length))); // NEW
+
 
 function findFriends(
     friends: Friend[],
@@ -39,5 +50,6 @@ function findFriends(
 ): Friend[] {
     return friends.filter(condition);
 }
+console.log("\n\nFIND FRIENDS")
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -2,7 +2,9 @@
 export interface Friend {
     name: string;
     phone: string;
-    age: number
+    dob? : Date;   // New optional
+    age: number;
+    interests? : string[]   // New optional
 }
 
 export interface Colleague {
